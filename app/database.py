f30958d0d395ddb22820abfb605c69249a4617f7
niravkadia <niravrajput0008@gmail.com>
2026-02-03 17:39:26 +0530

    Attched postgres

--- app/database.py
+++ app/database.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = DATABASE_URL = "postgresql+psycopg2://postgres:Nirav%40123456@localhost:5432/Fastapi_crud"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
